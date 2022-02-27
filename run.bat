4f4f88b3bbafc7e6c2b3528347854827e60b4942
panjinquan <panjinquan>
2022-02-27 14:57:35 +0800

    update

--- run.bat
+++ run.bat
@@ -1,3 +1,6 @@
+::conda create -n pytorch-py36 python==3.6.7
+::conda activate  pytorch-py36
+::pip install -r requirements.txt
 python run.py --image_file data/test_images
 python run.py --video_file data/videos/kunkun_cut.mp4
 python run.py --video_file 0
